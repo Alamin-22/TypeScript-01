c7be71932b554be39a4de714e2dd79e7b76581c6
Alamin-22 <mdalaminmollick@gmail.com>
2024-11-05 22:40:50 +0600

    updated

---
+++ Recap-1/src/Functions.ts
@@ -0,0 +1,9 @@
+{
+  // Types in Functions
+
+  const MultiplyFunc = (value1: number, value2: number): number => {
+    return value1 * value2;
+  };
+
+  const Result1 = MultiplyFunc(5, 5); // => as we already declared type for the arguments we cant provide any type of data we just have to provide only Number on that Function as an arguments
+}
---
+++ Recap-1/src/spread-operator.ts
@@ -0,0 +1,34 @@
+{
+  // spread operator and Destructuring
+
+  const numArr = [1, 2, 3, 4, 5, 6];
+
+  //   this is Spread Operator
+  const numArrUpdated = [...numArr, 7, 8, 9, 10];
+
+  console.log(numArrUpdated);
+
+  //   we can also implement Spread Operator to copy object as well
+
+  type PersonType = {
+    name: string;
+    Role: string;
+    StudentOf: string;
+    age: number;
+    Company: string;
+    Salary?: number;
+  };
+
+  //   external Type type for a object
+  const person1: PersonType = {
+    name: "Mr. Mollik",
+    Role: "MERN Stack Developer",
+    StudentOf: " Computer Science & Engineering",
+    age: 20,
+    Company: "Coder Squad",
+  };
+
+  const Person1Updated = { ...person1, salary: 1000000 };
+
+  console.log(Person1Updated);
+}
