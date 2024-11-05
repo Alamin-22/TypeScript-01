8aa06f91b70ce7e5a21b55be2d8da1020a438c80
Alamin-22 <mdalaminmollick@gmail.com>
2024-11-05 22:25:03 +0600

    asd

--- Recap-1/src/Basic-TypeScript.ts
+++ Recap-1/src/Basic-TypeScript.ts
@@ -1,3 +1,51 @@
 {
-  //
+  //Basic Types
+
+  const age: number = 20;
+  const Name: string = "mr. Mollik";
+  const IsActive: boolean = true;
+
+  //   for Arrays
+  const numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  //   tuples
+  const info: [number, string, boolean, object] = [
+    1,
+    "Mr. MOllik",
+    true,
+    { userId: 22, School: "RBHS" },
+  ];
+
+  //Objects in ts
+
+  //   //   inline Type type for a object
+  const person: {
+    name: string;
+    Role: string;
+    StudentOf: string;
+    age: number;
+    Company: string;
+  } = {
+    name: "Mr. Mollik",
+    Role: "MERN Stack Developer",
+    StudentOf: " Computer Science & Engineering",
+    age: 20,
+    Company: "Coder Squad",
+  };
+
+  type PersonType = {
+    name: string;
+    Role: string;
+    StudentOf: string;
+    age: number;
+    Company: string;
+  };
+
+  //   external Type type for a object
+  const person2: PersonType = {
+    name: "Mr. Mollik",
+    Role: "MERN Stack Developer",
+    StudentOf: " Computer Science & Engineering",
+    age: 20,
+    Company: "Coder Squad",
+  };
 }
